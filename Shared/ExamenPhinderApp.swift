ca3954cbaabe4ad81bd6d57585718e04bf91acb8
JesúsFranciscoLeyva <leyva527@gmail.com>
2022-06-08 18:37:44 -0600

    Initial Commit

---
+++ Shared/ExamenPhinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamenPhinderApp.swift
+//  Shared
+//
+//  Created by Jesús Francisco Leyva Juárez on 08/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ExamenPhinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
