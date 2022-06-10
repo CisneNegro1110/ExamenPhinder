c8915c3762f31ecbeb9dd1f779fe46dac2dee351
JesúsFranciscoLeyva <leyva527@gmail.com>
2022-06-10 16:23:31 -0600

    Content view

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,10 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+       firstView()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
