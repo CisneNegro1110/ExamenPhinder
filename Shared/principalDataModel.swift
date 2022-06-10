22c1c4f6e4f64712f4645415b34c3130c57141ad
JesúsFranciscoLeyva <leyva527@gmail.com>
2022-06-10 16:23:01 -0600

    modelo de datos

---
+++ Shared/principalDataModel.swift
@@ -0,0 +1,18 @@
+//
+//  DataModel.swift
+//  ExamenPhinder (iOS)
+//
+//  Created by Jesús Francisco Leyva Juárez on 10/06/22.
+//
+
+import Foundation
+
+struct principalDataModel {
+    var camara: Bool
+    var foto : Bool
+    var nombre : Bool
+    var numero : Bool
+    var fecha : Bool
+    var sexo : Bool
+    var color : Bool
+}
---
+++ Shared/secondaryDataModel.swift
@@ -0,0 +1,15 @@
+//
+//  secondaryDataModel.swift
+//  ExamenPhinder (iOS)
+//
+//  Created by Jesús Francisco Leyva Juárez on 10/06/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct secondaryDataModel {
+    var fecha: Date
+    var sexo: Int
+    var color: Color
+}
